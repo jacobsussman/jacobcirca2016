278812dc232b7c22e39194c3e31702c264626773
Jacob Sussman <sussmanjacob@gmail.com>
2016-04-04 01:37:46 -0400

    Added files via upload

---
+++ jacob.js
@@ -0,0 +1,18 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$(document).ready(function () {
+	"use strict";
+
+
+	
+});
+
+function intro() {
+	
+    var soundtrack = window.open("pages/soundtrack.html", "", "width=100, height=100");
+	soundtrack.blur();
+	var main =  window.open("pages/1.html", "", "width=500, height=500");
+	soundtrack.focus();
+	
+}
