4374ef7a48e78fcbb9bb74c36fde3f39f0a8fae5
jsussl <sussmanjacob@gmail.com>
2016-04-04 03:32:53 -0400

    updates
    new progress

--- jacob.js
+++ jacob.js
@@ -3,16 +3,48 @@
 
 $(document).ready(function () {
 	"use strict";
-
-
-	
+	var h = $(window).height(); 
+	var w = $(window).width(); 
+	var h2 = (h / 2);
+	var w2 = (w /2);
+	setTimeout(function() {
+		// XHR to request a JS and a CSS
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', 'pages/black.html');
+		xhr.send('');
+		xhr = new XMLHttpRequest();
+		xhr.open('GET', 'pages/soundtrack.html');
+		xhr.send('');
+		// preload image
+		new Image().src = "";
+	}, 1000);	
 });
 
+var count = 0;
 function intro() {
-	
-    var soundtrack = window.open("pages/soundtrack.html", "", "width=100, height=100");
-	soundtrack.blur();
-	var main =  window.open("pages/1.html", "", "width=500, height=500");
-	soundtrack.focus();
-	
+	if (count < 1) {
+		$(".slideDown").slideDown("fast");
+		count = 1;
+//		var eyy =  window.open("pages/black.html", "", "width=500, height=500");
+	} else {
+		var music = window.open("pages/soundtrack.html", "", "width='+w', height='+h'");
+		var page1 = window.open("pages/black.html", "",  "width='+w', height='+h'");
+		var page2 = window.open("pages/black.html", "", "width=500, height=500");
+		var page3 = window.open("pages/black.html", "", "width=500, height=500");
+		var page4 = window.open("pages/black.html", "", "width=500, height=500");
+		music.focus();
+	}
 }
+	
+	
+//	
+
+
+//}
+//    var soundtrack = window.open("pages/soundtrack.html", "", "width=100, height=100");
+//	soundtrack.blur();
+//	var page1 = window.open("pages/black.html", "", "width=500, height=500");
+//	var page2 = window.open("pages/black.html", "", "width=500, height=500");
+//	var page3 = window.open("pages/black.html", "", "width=500, height=500");
+//	var page4 = window.open("pages/black.html", "", "width=500, height=500");
+//	var soundtrack.focus();
